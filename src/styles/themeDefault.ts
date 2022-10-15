eac01dc0864d09ce60f3eb87b497e57b7d388290
Italo Araujo souza <italoasouzat.i@gmail.com>
2022-10-15 16:38:29 -0400

    create theme

---
+++ src/styles/themeDefault.ts
@@ -0,0 +1,35 @@
+export const themDefault = {
+    produto: {
+        red: {
+            'red_dark': '#BF3B44',
+            'red_mid': '#F3BABD',
+            'red_light': '#F4E6E7'
+        },
+        green: {
+            'green_dark': '#639339',
+            'green_mid': '#CBE4B4',
+            'green_light': '#E5F0DB'
+        },
+        base: {
+            'gray_1': '#1B1D1E',
+            'gray_2': '#333638',
+            'gray_3': '#5C6265',
+            'gray_4': '#B9BBBC',
+            'gray_5': '#DDDEDF',
+            'gray_6': '#EFF0F0',
+            'gray_7': '#FAFAFA',
+            'gray_8': '#DDDEDF',
+        }
+    },
+    font: {
+        regular: 'NunitoSans_400Regular',
+        bold: 'NunitoSans_700Bold',
+        lineHeight: '130%',
+        size: {
+            size_12: '12',
+            size_14: '14',
+            size_16: '16',
+            size_32: '32',
+        }
+    }
+}
